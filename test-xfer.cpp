6d8b2946c8ef631a19cdb162a3f33dece981c6ae
John Regehr <regehr@cs.utah.edu>
2019-12-08 12:41:02 -0700

    foo

--- test-xfer.cpp
+++ test-xfer.cpp
@@ -56,7 +56,12 @@ std::string KBString(KnownBits Known) {
   return s;
 }
 
-void test(Instruction::BinaryOps Op) {
+struct BinOp {
+  Instruction::BinaryOps Opcode;
+  bool nsw, nuw, exact;
+};
+
+void test(const BinOp &Op) {
   auto M = make_unique<Module>("", C);
   std::vector<Type *> T(2, Type::getIntNTy(C, W));
   FunctionType *FT = FunctionType::get(Type::getIntNTy(C, W), T, false);
@@ -71,7 +76,10 @@ void test(Instruction::BinaryOps Op) {
 
   KnownBits K0(W), K1(W);
   while (true) {
-    auto I = BinaryOperator::Create(Op, maskKnown(K0, Args[0]), maskKnown(K1, Args[1]));
+    auto I = BinaryOperator::Create(Op.Opcode, maskKnown(K0, Args[0]), maskKnown(K1, Args[1]));
+    I->setHasNoSignedWrap(Op.nsw);
+    I->setHasNoUnsignedWrap(Op.nuw);
+    I->setIsExact(Op.exact);
     B.Insert(I);
     auto R = B.CreateRet(I);
     KnownBits KB = computeKnownBits(I, DL);
@@ -98,32 +106,51 @@ void test(Instruction::BinaryOps Op) {
       }
     }
   }
-  
-  outs() << "total cases = " << Cases << "\n";
-  outs() << "total known bits = " << Bits << "\n";
+
+  outs() << Instruction::getOpcodeName(Op.Opcode) << " ";
+  if (Op.nsw)
+    outs() << "nsw ";
+  if (Op.nuw)
+    outs() << "nuw ";
+  if (Op.exact)
+    outs() << "exact ";
+  outs() << "\n";
+  outs() << "  total known bits = " << Bits << " (" << Cases << " cases)\n";
 }
 
-  std::vector<Instruction> Ops {
-                                   Instruction::Add,
-#if 0
-                                   BinaryOperator::Sub,
-                                   BinaryOperator::Mul,
-                                   BinaryOperator::UDiv,
-                                   BinaryOperator::SDiv,
-                                   BinaryOperator::URem,
-                                   BinaryOperator::SRem,
-                                   BinaryOperator::Shl,
-                                   BinaryOperator::LShr,
-                                   BinaryOperator::AShr,
-                                   BinaryOperator::And,
-                                   BinaryOperator::Or,
-                                   BinaryOperator::Xor,
-#endif
-                                   };
+std::vector<BinOp> Ops {
+  { Instruction::Add, false, false, false },
+  { Instruction::Add, true, false, false },
+  { Instruction::Add, false, true, false },
+  { Instruction::Add, true, true, false },
+  { Instruction::Sub, false, false, false },
+  { Instruction::Sub, true, false, false },
+  { Instruction::Sub, false, true, false },
+  { Instruction::Sub, true, true, false },
+  { Instruction::Mul, false, false, false },
+  { Instruction::Mul, true, false, false },
+  { Instruction::Mul, false, true, false },
+  { Instruction::Mul, true, true, false },
+  { Instruction::UDiv, false, false, false },
+  { Instruction::UDiv, false, false, true },
+  { Instruction::SDiv, false, false, false },
+  { Instruction::SDiv, false, false, true },
+  { Instruction::URem, false, false, false },
+  { Instruction::SRem, false, false, false },
+  { Instruction::Shl, false, false, false },
+  { Instruction::LShr, false, false, false },
+  { Instruction::LShr, false, false, true },
+  { Instruction::AShr, false, false, false },
+  { Instruction::AShr, false, false, true },
+  { Instruction::And, false, false, false },
+  { Instruction::Or, false, false, false },
+  { Instruction::Xor, false, false, false },
+};
   
 } // namespace
 
 int main(void) {
-  test(BinaryOperator::URem);
+  for (auto &Op : Ops)
+    test(Op);
   return 0;
 }
